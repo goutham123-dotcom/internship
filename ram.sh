9aa63e50e704954b0858741c02b953e1227f4cce
goutham123-dotcom <gouthamkrishna860@gmail.com>
2023-01-23 17:01:51 +0530

    Added files

--- ram.sh
+++ ram.sh
@@ -2,6 +2,6 @@
 
 while true; do
     RAM=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"RAM usage: '"$RAM"'"}' https://hooks.slack.com/services/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
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"RAM usage: '"$RAM"'"}' https://hooks.slack.com/services/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
     sleep 600
 done
